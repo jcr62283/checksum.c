f5cc9cd59d837ce9e8959a6e4ce35d4423abb882
jcr62283 <jericho.raz.584@my.csun.edu>
2021-02-09 19:40:46 -0800

    First commit

---
+++ checksum.c
@@ -0,0 +1,41 @@
+/********************************/
+/* Program Name:                */
+/* Author:                      */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+#define max_int (255)
+#define byte (char)
+
+int main(int argc, char* argv[], char** envp) {
+
+    int count = 10;
+    int sum = 0;
+    byte checksum;
+    byte complement;
+
+    byte[] num = read(1, sizeof(byte), 10);
+
+    for (int i = 0; i < count; i++)
+    {
+        fprintf(num[i]);
+    }
+
+    checksum = 0;
+    complement = 0;
+    /* the following is the prototype for the read system call */
+    /* int read(int fildes, void *buf, size_t nbyte);  */
+    fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+    if (checksum != complement) {
+        fprintf(stderr, "Error Detected!\n");
+        return 1;
+    }
+    return 0;
+}
